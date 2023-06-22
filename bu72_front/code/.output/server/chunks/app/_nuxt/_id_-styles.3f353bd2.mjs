23d6537e83201723728a50340556ba03f7865d66
Sergey <1@php-cat.com>
2023-06-23 04:03:49 +0500

    123

---
+++ bu72_front/code/.output/server/chunks/app/_nuxt/_id_-styles.3f353bd2.mjs
@@ -0,0 +1,6 @@
+const _id__vue_vue_type_style_index_0_scoped_7260c54a_lang = ".mh[data-v-7260c54a]{border:1px solid green;display:block;max-height:60px;overflow:auto;padding:5px}";
+
+const _id_Styles_3f353bd2 = [_id__vue_vue_type_style_index_0_scoped_7260c54a_lang];
+
+export { _id_Styles_3f353bd2 as default };
+//# sourceMappingURL=_id_-styles.3f353bd2.mjs.map
---
+++ bu72_front/code/.output/server/chunks/app/styles.mjs
@@ -0,0 +1,10 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "pages/cat/[id].vue": () => import('./_nuxt/_id_-styles.3f353bd2.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.6b5b5ff2.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.a5c3f351.mjs').then(interopDefault),
+  "layouts/default.vue": () => import('./_nuxt/default-styles.3cb0e544.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map

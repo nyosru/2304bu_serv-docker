7142a978398d7d2af2b14a6d62fc645a93932162
Sergey <1@php-cat.com>
2024-06-12 22:27:11 +0500

    16

--- cron/entrypoint.sh
+++ cron/entrypoint.sh
@@ -3,5 +3,13 @@
 # Запускаем cron в фоновом режиме
 cron
 
+# Печатаем crontab для отладки
+echo "Current crontab:"
+crontab -l
+
+# Печатаем процессы для отладки
+echo "Current processes:"
+ps aux
+
 # Печатаем логи cron
 tail -f /var/log/cron/cron.log

2030613aa5e2ab3f35ecf42d6111619b1bf17c24
Sergey <1@php-cat.com>
2024-07-26 00:22:31 +0500

    5-02

---
+++ ftp-server2/add_ftp_users.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FTP_USERS_FILE=$1
+
+if [ -f "$FTP_USERS_FILE" ]; then
+  while IFS='|' read -r user pass; do
+    echo "Creating user: $user"
+    useradd -m -s /bin/false $user
+    echo -e "$pass\n$pass" | passwd $user
+  done < "$FTP_USERS_FILE"
+else
+  echo "FTP users file not found: $FTP_USERS_FILE"
+  exit 1
+fi

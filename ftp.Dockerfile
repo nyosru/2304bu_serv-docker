a95d8aaf8ffcb67a314b6d343878da893b27cfec
Sergey <1@php-cat.com>
2024-11-13 03:23:58 +0500

    12-60

--- ftp.Dockerfile
+++ ftp.Dockerfile
@@ -1,12 +1,12 @@
 FROM fauria/vsftpd
 
-# Копируем файлы конфигурации
+# Копируем конфигурационные файлы
 COPY ftp.vsftpd.conf /etc/vsftpd/vsftpd.conf
 COPY ftp.virtual_users.txt /tmp/virtual_users.txt
-
-# Копируем и назначаем права для скрипта
 COPY ftp.add_virtual_users.sh /usr/local/bin/add_virtual_users.sh
+
+# Назначаем права на выполнение скрипта
 RUN chmod +x /usr/local/bin/add_virtual_users.sh
 
-# Используем ENTRYPOINT для запуска скрипта и сервера
-ENTRYPOINT ["/usr/local/bin/add_virtual_users.sh", "&&", "/usr/sbin/vsftpd", "/etc/vsftpd/vsftpd.conf"]
+# Используем ENTRYPOINT для запуска скрипта один раз и затем запускаем FTP-сервер
+ENTRYPOINT ["/bin/bash", "-c", "/usr/local/bin/add_virtual_users.sh && /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf"]
--- ftp.add_virtual_users.sh
+++ ftp.add_virtual_users.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-set -e
-
-DB_PATH="/etc/vsftpd/vsftpd_login.db"
-
-if [ ! -f "$DB_PATH" ]; then
-    touch $DB_PATH
-    chmod 600 $DB_PATH
-fi
-
 echo "Adding virtual users..."
 
-while read line; do
-    username=`echo $line | cut -d':' -f1`
-    password=`echo $line | cut -d':' -f2`
-
-    if [ -z "$username" ] || [ -z "$password" ]; then
-        continue
-    fi
+# Добавляем пользователей из файла /tmp/virtual_users.txt
+while IFS= read -r line
+do
+    username=$(echo "$line" | cut -d':' -f1)
+    password=$(echo "$line" | cut -d':' -f2)
 
     echo "Adding user: $username with password: $password"
-    echo $username >> /etc/vsftpd/virtual_users.txt
-    echo $password >> /etc/vsftpd/virtual_users_passwd.txt
-    echo -e "$username\n$password\n" | pam_password dbm -c >& /dev/null
+
+    # Добавление пользователя (замените команду на нужную)
+    # Например, создание виртуального пользователя и установка пароля
+    useradd -m "$username" -p "$(openssl passwd -1 "$password")"
 done < /tmp/virtual_users.txt
 
-echo "Virtual users added successfully."
+echo "Finished adding virtual users."
